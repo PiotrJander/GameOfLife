7566125855c1936dde6ca53c1da92146dc86afbf
PiotrJander <pjander2@gmail.com>
2014-11-21 11:36:00 +0000

    Version as of Friday morning

--- src/Main.hs
+++ src/Main.hs
@@ -2,21 +2,37 @@ import Grid
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort(ViewPort)
 import Data.Matrix
+import Data.List
 import Text.Printf
 import System.Environment
+import System.Directory
 
 type Time  = Float
 type Coors = (Float, Float)
 
 main = do
 	args <- getArgs
+	if head args == "options"
+		then available
+		else life args
+
+life :: [String] -> IO ()
+life args = do
 	let
 		stepsPerSec = read (head args) :: Int
 		patternName = last args
-		fileName = printf "../Patterns/%s.rle" patternName
+		fileName = printf "../patterns/%s.rle" patternName
 	pattern <- fromFile fileName
 	let d = InWindow "Game of Life" (700, 700) (10, 10)
 	simulate d black stepsPerSec pattern render tick'
+
+available :: IO ()
+available = do
+	files <- getDirectoryContents "../patterns"
+	let
+		beforeDot = fst . break (== '.')
+		patterns = map beforeDot . filter (".rle" `isSuffixOf`) $ files
+	mapM_ putStrLn patterns
 
 render :: Grid -> Picture
 render = translate (-200) (-200) . scale 5 5 . pictures . map f . findSquares
